1407407c4628791a567393b5a6169caf76b3f55f
Abildtrup <aabildtruphansen@gmail.com>
2019-06-12 13:27:56 +0200

    Finished ex3

--- Ex1/RNG.py
+++ Ex1/RNG.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import chi2
+    from scipy.stats import chi2
 from scipy.stats import norm
 
 #########################################################
--- Ex3/Ex3.py
+++ Ex3/Ex3.py
@@ -7,9 +7,11 @@ Created on Fri Jun  7 14:36:03 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 from math import pi
 from scipy.stats import chi2
 from scipy.stats import norm
+from scipy.stats import t
 
 
 ###########################################################
@@ -31,7 +33,6 @@ Z1 = np.sqrt(-2*np.log(U1)) * np.cos(2*pi*U2)
 plt.hist(Z1, 20)
 
 # Pareto with beta=1 and k=2.05, 2.5, 3, 4.
-
 def Pareto(beta, k, U1):
     P1 = beta*(np.power(U1, -(1.0/k)))
     plt.hist(P1, 40)
@@ -64,4 +65,45 @@ np.std(P4)**2 # Empirical variance of pareto distribution.
 
 #############################################################
 # We generate   100 95% confidence intervals:
+
+U = np.random.rand(100*10)
+U_helper = np.random.rand(100*10)
+Z = np.sqrt(-2*np.log(U)) * np.cos(2*pi*U_helper)
+
+plt.hist(Z)
+# mean estimate using 10 observations:
+
+mu_list = np.zeros((100, 3))
+var_list = np.zeros((100, 3))
+
+for i in range(100):
+    Z_list = Z[10*i:(10*i)+10]
+    mu = 0.1*sum(Z_list)
+    var = 1.0/(10-1)*sum((Z_list-mu*np.ones(10))**2)
+    s = math.sqrt(var/10)
+    
+    mu_lower = mu - 1.96*s
+    mu_upper = mu + 1.96*s
+    
+    var_upper= ((10.0-1)*var)/(chi2.ppf(0.025, 9))
+    var_lower= ((10.0-1)*var)/(chi2.ppf(0.975, 9))
+    
+    mu_list[i, :] = [mu, mu_lower, mu_upper]
+    var_list[i, :] = [var, var_lower, var_upper]
+    
+    #print("Mean confidence interval: [" + str(mu_lower) + ", " + str(mu_upper) + "]")
+    #print("Var confidence interval: [" + str(var_lower) + ", " + str(var_upper) + "]")
+
+# Mean estimates with lower and upper bound;
+mu_list
+# Variance esstiamte with lower and upper bound:
+var_list
+
+# The mean estimates varies a lot.
+# So does the variance estimates.
+# This is due to the fact, that only 10 observations are used.
+# Note that:
+print("Average Mean estimate: " + str(sum(mu_list[:,0])/100))
+print("Average Variance estimate: " + str(sum(var_list[:,0])/100))
+# are both close to the true value.
 
