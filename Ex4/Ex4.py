efe202ead52549be65b9a773d5fd32d032c23945
Mikkel Jordahn <mikkeljordahn@hotmail.com>
2019-06-13 14:23:59 +0200

    more exercise 4

--- Ex4/Ex4.py
+++ Ex4/Ex4.py
@@ -7,12 +7,16 @@ import bisect
 
 def simulation(_lambda, _service_time, runtimes, arrival, service):
     theta_hats = []
+    burn = 4000
     for i in range(0, runtimes):
         theta_hat = 0
         offered_traffic = int(sys.argv[1])
         service_unit = [False]*int(sys.argv[2])
         event_list = []
         new_sample = 0
+
+        blocked = 0
+        time = 0
         if arrival == "poisson":
             new_sample = np.random.poisson(lam=_lambda)
         elif arrival == "erlang":
@@ -26,11 +30,10 @@ def simulation(_lambda, _service_time, runtimes, arrival, service):
                 new_sample = np.random.exponential(scale=1/5)
         arrival_time = new_sample
         event_list.append((arrival_time, 'a', None))
-
         customers = 1
-        blocked = 0
 
-        while customers <= offered_traffic:
+
+        while customers <= offered_traffic+burn-1:
             next_event = event_list.pop(0)
             time = next_event[0]
             if next_event[1] == "a":
@@ -47,7 +50,21 @@ def simulation(_lambda, _service_time, runtimes, arrival, service):
                             bisect.insort_left(event_list,(new_end_time, 'd', i))
                             service_unit[i] = True
                             break
-                    elif i == len(service_unit)-1:
+                        elif service == "pareto1":
+                            shape = 1.05
+                            scale = _service_time*(shape-1)/shape
+                            new_end_time = time + (np.random.pareto(shape)+1)*scale
+                            bisect.insort_left(event_list,(new_end_time, 'd', i))
+                            service_unit[i] = True
+                            break
+                        elif service == "pareto2":
+                            shape = 2.05
+                            scale = _service_time*(shape-1)/shape
+                            new_end_time = time + (np.random.pareto(shape)+1)*scale
+                            bisect.insort_left(event_list,(new_end_time, 'd', i))
+                            service_unit[i] = True
+                            break
+                    elif i == len(service_unit)-1 and customers>burn:
                         blocked = blocked + 1
                 new_sample = 0
                 if arrival == "poisson":
@@ -61,12 +78,14 @@ def simulation(_lambda, _service_time, runtimes, arrival, service):
                         new_sample = np.random.exponential(scale=1/0.8333)
                     else:
                         new_sample = np.random.exponential(scale=1/5)
+                elif arrival == "chi2":
+                    new_sample = np.random.chisquare(df=1)
                 new_arrival_time = time + new_sample
                 bisect.insort_right(event_list, (new_arrival_time, 'a', None))
+
                 customers=customers+1
             elif next_event[1] == 'd':
                 service_unit[next_event[2]] = False
-
         theta_hat = float(blocked)/float(offered_traffic)
         theta_hats.append(theta_hat)
     theta_bar = 0
@@ -78,8 +97,8 @@ def simulation(_lambda, _service_time, runtimes, arrival, service):
         sum_hats = sum_hats + theta**2
     std = math.sqrt(1/(runtimes-1)*(sum_hats-theta_bar**2*runtimes))
     sqrt_n = math.sqrt(int(runtimes))
-    lower_bound_confidence = theta_bar + std/sqrt_n*t.ppf(0.025, df=runtimes-1)*(runtimes-1)
-    upper_bound_confidence = theta_bar + std/sqrt_n*t.ppf(0.975, df=runtimes-1)*(runtimes-1)
+    lower_bound_confidence = theta_bar + std/sqrt_n*t.ppf(0.05, df=runtimes-1)*(runtimes-1)
+    upper_bound_confidence = theta_bar + std/sqrt_n*t.ppf(0.95, df=runtimes-1)*(runtimes-1)
     sum_theoretical = 0
     A = _lambda*_service_time
     for i in range (0, int(sys.argv[2])+1):
@@ -112,6 +131,26 @@ def main():
     print("Upper bound: " + str(upper_bound_confidence))
     print("-----------------------------------------------------------------------------\n\n")
 
+    theta_bar, theoretical_fraction, std, lower_bound_confidence, upper_bound_confidence =  simulation(_lambda, _service_time, runtimes, "poisson", "pareto1")
+
+    print("-------------------Poisson arrival and pareto1 service-------------------")
+    print("Estimator: " + str(theta_bar))
+    print("Theoretical fraction: " + str(theoretical_fraction))
+    print("Standard deviation: " + str(std))
+    print("Lower bound: " + str(lower_bound_confidence))
+    print("Upper bound: " + str(upper_bound_confidence))
+    print("-----------------------------------------------------------------------------\n\n")
+
+    theta_bar, theoretical_fraction, std, lower_bound_confidence, upper_bound_confidence =  simulation(_lambda, _service_time, runtimes, "poisson", "pareto2")
+
+    print("-------------------Poisson arrival and pareto2 service-------------------")
+    print("Estimator: " + str(theta_bar))
+    print("Theoretical fraction: " + str(theoretical_fraction))
+    print("Standard deviation: " + str(std))
+    print("Lower bound: " + str(lower_bound_confidence))
+    print("Upper bound: " + str(upper_bound_confidence))
+    print("-----------------------------------------------------------------------------\n\n")
+    exit
     #Variance 1 and mean 1
 
     theta_bar, theoretical_fraction, std, lower_bound_confidence, upper_bound_confidence =  simulation(_lambda, _service_time, runtimes, "erlang", "exponential")
@@ -128,6 +167,26 @@ def main():
     theta_bar, theoretical_fraction, std, lower_bound_confidence, upper_bound_confidence =  simulation(_lambda, _service_time, runtimes, "erlang", "constant")
 
     print("-------------------Erlang arrival and constant service-------------------")
+    print("Estimator: " + str(theta_bar))
+    #print("Theoretical fraction: " + str(theoretical_fraction))
+    print("Standard deviation: " + str(std))
+    print("Lower bound: " + str(lower_bound_confidence))
+    print("Upper bound: " + str(upper_bound_confidence))
+    print("-----------------------------------------------------------------------------\n\n")
+
+    theta_bar, theoretical_fraction, std, lower_bound_confidence, upper_bound_confidence =  simulation(_lambda, _service_time, runtimes, "erlang", "pareto1")
+
+    print("-------------------Erlang arrival and pareto1 service-------------------")
+    print("Estimator: " + str(theta_bar))
+    #print("Theoretical fraction: " + str(theoretical_fraction))
+    print("Standard deviation: " + str(std))
+    print("Lower bound: " + str(lower_bound_confidence))
+    print("Upper bound: " + str(upper_bound_confidence))
+    print("-----------------------------------------------------------------------------\n\n")
+
+    theta_bar, theoretical_fraction, std, lower_bound_confidence, upper_bound_confidence =  simulation(_lambda, _service_time, runtimes, "erlang", "pareto2")
+
+    print("-------------------Erlang arrival and pareto2 service-------------------")
     print("Estimator: " + str(theta_bar))
     #print("Theoretical fraction: " + str(theoretical_fraction))
     print("Standard deviation: " + str(std))
@@ -156,6 +215,40 @@ def main():
     print("Upper bound: " + str(upper_bound_confidence))
     print("-----------------------------------------------------------------------------\n\n")
 
+    theta_bar, theoretical_fraction, std, lower_bound_confidence, upper_bound_confidence =  simulation(_lambda, _service_time, runtimes, "hyperexponential", "pareto1")
 
+    print("-------------------Hyperexponential arrival and pareto1 service-------------------")
+    print("Estimator: " + str(theta_bar))
+    #print("Theoretical fraction: " + str(theoretical_fraction))
+    print("Standard deviation: " + str(std))
+    print("Lower bound: " + str(lower_bound_confidence))
+    print("Upper bound: " + str(upper_bound_confidence))
+    print("-----------------------------------------------------------------------------\n\n")
+
+    theta_bar, theoretical_fraction, std, lower_bound_confidence, upper_bound_confidence =  simulation(_lambda, _service_time, runtimes, "hyperexponential", "pareto2")
+
+    print("-------------------Hyperexponential arrival and pareto2 service-------------------")
+    print("Estimator: " + str(theta_bar))
+    #print("Theoretical fraction: " + str(theoretical_fraction))
+    print("Standard deviation: " + str(std))
+    print("Lower bound: " + str(lower_bound_confidence))
+    print("Upper bound: " + str(upper_bound_confidence))
+    print("-----------------------------------------------------------------------------\n\n")
+
+    theta_bar, theoretical_fraction, std, lower_bound_confidence, upper_bound_confidence =  simulation(_lambda, _service_time, runtimes, "chi2", "exponential")
+
+
+    print("-------------------Chi2 arrival and exponential service--------------------")
+    print("Estimator: " + str(theta_bar))
+    print("Standard deviation: " + str(std))
+    print("Lower bound: " + str(lower_bound_confidence))
+    print("Upper bound: " + str(upper_bound_confidence))
+    print("---------------------------------------------------------------------------------------\n\n")
+
+    #Variance 2 - should be the worst of them all
+
+
+    #Variance of pareto with k = 1.05 is infinite! Mean is 8.
+    #Variance of pareto with k = 2.05 is 1.56! Mean is 8.
 if __name__ == "__main__":
     main()
